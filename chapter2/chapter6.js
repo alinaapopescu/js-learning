41aa2890b5f0c76e355948c7a1e5b59058576695
alinutz03 <popescu.alina@79@yahoo.com>
2024-06-19 14:48:06 +0300

    Basic selectors

---
+++ chapter2/chapter6.js
@@ -0,0 +1,21 @@
+
+// get an element by ID
+const title = document.getElementById('#page-title');
+
+
+// get an element by their class name
+const errors = document.getElementsByClassName('error');
+console.log(errors);
+console.log(errors[0]);
+
+
+
+// NU MERGE FOREACH PE COLECTII
+// errors.forEach(element => {
+//   console.log(element)
+// });
+
+
+// get an element by tag name
+const params = document.getElementsByTagName('p');
+console.log(params);
